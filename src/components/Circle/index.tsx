c2c2ebd90de63c19d1f087983e0657269766e746
Fernando Arruda <fernandojs006@gmail.com>
2022-03-21 20:37:10 -0300

    arrumando o bagulho da build

--- src/components/Circle/index.tsx
+++ src/components/Circle/index.tsx
@@ -1,6 +1,5 @@
 import type { NextPage } from "next";
 import { Icon, useColorMode } from "@chakra-ui/react";
-import { BsPlusCircle } from "react-icons/bs";
 import styles from "./style.module.css";
 
 type CircleProps = {
@@ -11,20 +10,6 @@ type CircleProps = {
 
 const Circle: NextPage<CircleProps> = ({ isActive, onClick, add }) => {
   const { colorMode } = useColorMode();
-
-  if (add) {
-    return (
-      <Icon
-      m={.3}
-        as={BsPlusCircle}
-        className={styles.circle}
-        onClick={onClick}
-        style={{ cursor: "pointer" }}
-        viewBox="0 0 200 200"
-        color={colorMode === "light" ? (isActive ? "black" : "GrayText") : isActive ? "#75bdff" : "#4c6589"}
-      ></Icon>
-    );
-  }
 
   return (
     <Icon
